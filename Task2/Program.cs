dd798e15cc314355ba80f2ccc035b590c80a10b4
pyshkin <pyshkin_pavel93@mail.ru>
2023-09-21 19:54:31 +0300

    Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,1 +1,16 @@
 ﻿
+Console.WriteLine("Введите два числа:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (number1>number2)
+{
+    int Max = number1;
+    Console.WriteLine($"max->{Max}");
+}
+else
+{
+    int Max=number2;
+    Console.WriteLine($"max->{Max}");
+}
