475cc1c26a8357ed61517e229e5bf2f4354c6631
pyshkin <pyshkin_pavel93@mail.ru>
2023-09-24 16:56:28 +0300

    task8

---
+++ Task8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int index = 1; index <= number; index++)
+{
+    if (index % 2 == 0)
+    {
+        Console.WriteLine($"{index}");
+    }
+}
