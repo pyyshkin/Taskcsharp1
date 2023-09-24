dda358fff2101f50c9b5c8e9f83becdd598dc549
pyshkin <pyshkin_pavel93@mail.ru>
2023-09-24 16:56:07 +0300

    task6

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number%2==0)
+{
+    Console.WriteLine($"да");
+} 
+else
+{
+    Console.WriteLine($"нет");
+}
