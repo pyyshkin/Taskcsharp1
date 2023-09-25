d32afdc5918f0c9ac44c2f634e486dba1def9ca6
pyshkin <pyshkin_pavel93@mail.ru>
2023-09-25 19:44:40 +0300

    task10

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number>100 && number<=1000)
+{
+    int secondNumber=number/10%10;
+    Console.WriteLine($"Вторая цифра ->{secondNumber}");
+}
+else
+{
+    Console.WriteLine("Введенно неправильное значение");
+}
