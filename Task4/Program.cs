576511b0862452aa5b199cb4896bcc1a91ad5fc7
pyshkin <pyshkin_pavel93@mail.ru>
2023-09-25 19:20:42 +0300

    task4+

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -8,18 +8,15 @@ if (number1 > number2)
 {
     if (number1 > number3) 
     {
-        int max=number1;
         Console.WriteLine($"{number1}");
     }   
 }
 else if (number2>number3)
 {
-   int max=number2;
    Console.WriteLine($"{number2}");    
 }
 else
 {
-    int max=number3;
     Console.WriteLine($"{number3}");
 }
 
