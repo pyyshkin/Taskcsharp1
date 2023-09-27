b80d9d641f43e41c2cb359e62f1ba1fb5b8ba5f9
pyshkin <pyshkin_pavel93@mail.ru>
2023-09-27 19:05:28 +0300

    task13

---
+++ Task13/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int ThirdNumber(int number)
+{
+    while (number>999)
+    {
+        number /=10;
+    }
+    return number%10;
+}
+bool ValidateNumber(int number)
+{
+    if (number<100)
+    {
+        Console.WriteLine("Нет третьей цифры.");
+        return false;
+    }
+    return true;
+}
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (ValidateNumber(number))
+{
+    Console.WriteLine(ThirdNumber(number));
+}
