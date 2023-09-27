926044b6df0bed60eedd5f68bd9e78639d1bf75d
pyshkin <pyshkin_pavel93@mail.ru>
2023-09-27 19:37:00 +0300

    task15

---
+++ Task15/Program.cs
@@ -0,0 +1,31 @@
+﻿
+bool ThisIsWeekend(int day)
+{
+    if (day > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool ValidateDay(int number)
+{
+    if (number > 0 && number <= 7)
+    {
+        return true;
+    }
+    Console.WriteLine("Такого дня нет!");
+    return false;
+}
+
+Console.WriteLine("Введите день недели:");
+int day = Convert.ToInt32(Console.ReadLine());
+if (ValidateDay(day))
+{    
+    if (ThisIsWeekend(day))
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    Console.WriteLine("Рабочий");
+}
