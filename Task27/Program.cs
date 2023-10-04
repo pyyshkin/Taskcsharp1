74397d91c53bd382328871c992b82d4bc1d27f2d
pyshkin <pyshkin_pavel93@mail.ru>
2023-10-04 20:21:20 +0300

    task27

---
+++ Task27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumAllNumber(int number)
+{
+    int result=0;
+    while (number>0)
+    {
+        result+=number%10;
+        number=number/10;
+    }
+    return result;
+}
+
+Console.WriteLine("Ввелите число:");
+int number=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма чисел цифры {number}-> {SumAllNumber(number)}");
