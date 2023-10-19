10fc7e0704b44ef4abea49b6a748f2312d991bb0
pyshkin <pyshkin_pavel93@mail.ru>
2023-10-19 19:37:28 +0300

    task66

---
+++ Task66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumbers(int m, int n)
+{
+    if (m==n) return n;
+    else return m+SumNumbers(m+1,n);
+}
+
+Console.WriteLine("Введите первое число:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N -> {SumNumbers(m,n)}");
